72fba2dd7c2f425b0168bfd15bd2cf41a7330676
wkoslik <wkoslik@gmail.com>
2021-02-10 14:02:50 -0500

    Whitney Koslik component

---
+++ src/AboutMe/WhitneyKoslik.js
@@ -0,0 +1,16 @@
+//this is whitney koslik's about me component
+
+import React from 'react'
+
+const WhitneyKoslik = () => {
+    return (
+        <div className="about-me">
+            <img src="https://media-exp1.licdn.com/dms/image/C4D35AQFMp_YlU8mVjg/profile-framedphoto-shrink_200_200/0/1612547804297?e=1613070000&v=beta&t=GFLv8bXC-mT2CDkWn4eDlj9B3azz-h_OXiBn6D7GsUM" alt="Whitney Koslik" />
+            <h3>About Whitney</h3>
+            <p>I am a web developer, a rock climber, and a home-brewer who sees the many possibilities from taking a road less traveled. I provide stability for my team — domestic and international — with problem proof schedules and communication strategies. I give my team a sense of certainty with my open-minded considerations and initiatives to continue to learn. </p>
+
+        </div>
+    )
+}
+
+export default WhitneyKoslik
