60e41791cfc153006089a83a50c06af25d9e63fc
wkoslik <wkoslik@gmail.com>
2021-02-10 14:03:25 -0500

    update whitney component image

--- src/AboutMe/WhitneyKoslik.js
+++ src/AboutMe/WhitneyKoslik.js
@@ -5,7 +5,7 @@ import React from 'react'
 const WhitneyKoslik = () => {
     return (
         <div className="about-me">
-            <img src="https://media-exp1.licdn.com/dms/image/C4D35AQFMp_YlU8mVjg/profile-framedphoto-shrink_200_200/0/1612547804297?e=1613070000&v=beta&t=GFLv8bXC-mT2CDkWn4eDlj9B3azz-h_OXiBn6D7GsUM" alt="Whitney Koslik" />
+            <img src="https://media-exp1.licdn.com/dms/image/C4E03AQHNdH2jAmz-lA/profile-displayphoto-shrink_800_800/0/1568035496608?e=1618444800&v=beta&t=cU6X51PfA6JGkk0_r-bGmc3QTbX2ShCrfzzAgXL8mEU" alt="Whitney Koslik" />
             <h3>About Whitney</h3>
             <p>I am a web developer, a rock climber, and a home-brewer who sees the many possibilities from taking a road less traveled. I provide stability for my team — domestic and international — with problem proof schedules and communication strategies. I give my team a sense of certainty with my open-minded considerations and initiatives to continue to learn. </p>
 
